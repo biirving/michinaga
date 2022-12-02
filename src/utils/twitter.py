2ea7cabf6332e11e393880c4eb236584c6262468
Lysander-curiosum <slashpowben@gmail.com>
2022-12-02 00:59:43 -0500

    edit

--- src/utils/twitter.py
+++ src/utils/twitter.py
@@ -4,8 +4,3 @@ Class to scrape relevant tweets from web. Will be important for actually deployi
 
 """
 
-# use twitter api keys, can you scrape historic contributions? 
-
-# can you deploy this shit to conquer the world
-
-# also, I need to be able to use normalized price data, build it from stuff out there in the world
